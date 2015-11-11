4d28c32dd3e3ed4fafccca478734e32a7ee269a7
Amber Houle <amber.houle3@gmail.com>
2015-11-10 21:50:21 -0500

    Secret Santa Generator can add participants via form

--- form-generator/index.js
+++ form-generator/index.js
@@ -1,5 +1,25 @@
 define([], function () {
-  return function(){
+  return function(inputFormat){
+    var $form = $("<form>").addClass("form-inline");
 
+    if(Array.isArray(inputFormat)) {
+
+      inputFormat.forEach(function (input) {
+          var $formElement = $("<div>").addClass("form-group");
+          var $label = $("<label>").attr("for", "'input"+input.type+"'").text(input.type);
+
+          var $inputElement = $("<input id='input"+input.type+"'>").addClass("form-control");
+          $inputElement.attr("type", input.type);
+          $inputElement.attr("placeholder", input.value);
+
+          $formElement.append($label);
+          $formElement.append($inputElement);
+
+          $form.append($formElement);
+      });
+
+    }
+
+    return $form[0];
   };
 });
--- form-generator/spec.js
+++ form-generator/spec.js
@@ -1,9 +1,42 @@
 define(["./index"], function (FormGenerator) {
-  
+
    describe("A form generator", function () {
 
      it("is defined", function() {
        expect(FormGenerator).toBeDefined();
+     });
+
+     it("is a function", function () {
+       expect(typeof FormGenerator).toBe("function");
+     });
+
+     describe("instance", function () {
+
+       it("is a DOM element", function () {
+          expect(FormGenerator()).toExist();
+        });
+
+       it("it should have a form-inline class", function () {
+          expect(FormGenerator()).toHaveClass("form-inline");
+       });
+
+       it("it should have a an input box for name and e-mail", function () {
+          var inputFormat = [
+            { type: "name", value: "Amber Awesome" },
+            { type: "email", value: "amber.awesome@example.com"}
+          ]
+
+          var form = FormGenerator(inputFormat);
+
+          var firstFormInput = form.querySelector("input:first-of-type");
+          var secondFormInput = form.querySelector("input:last-of-type");
+
+          expect(firstFormInput).toHaveClass("form-control");
+          expect(firstFormInput).toHaveId("inputname");
+          expect(firstFormInput.getAttribute("type")).toEqual("name");
+          expect(firstFormInput.getAttribute("placeholder")).toEqual("Amber Awesome");
+       });
+
      });
 
    });
