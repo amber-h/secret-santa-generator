907734c8c17317e6ff942d54a1a90787b8903510
Amber Houle <amber.houle3@gmail.com>
2015-11-14 20:45:59 -0500

    Adding basic pair generator in ruby server

--- server/lib/pair_generator.rb
+++ server/lib/pair_generator.rb
@@ -1,10 +1,25 @@
 class PairGenerator
 
   def generate_pairs participants
-    pairs = [
-      { :purchaser => { :email => participants.first[:email], :recipient => { :email => participants.first[:email] } } }
-    ]
+    return [format_pair(participants.first[:email], participants.first[:email])] if participants.size == 1
+
+    potential_recipients = participants
+    pairs = []
+
+    recipient_index = Random.rand(potential_recipients.length)
+    participants.each do |participant|
+      recipient_index = Random.rand(potential_recipients.length) until potential_recipients[recipient_index][:email] != participant[:email]
+      pairs.push(format_pair(participant[:email], potential_recipients[recipient_index][:email]))
+      potential_recipients.delete(recipient_index)
+    end
+
     return pairs
+  end
+
+  private
+
+  def format_pair purchaser_email, recipient_email
+    { :purchaser => { :email => purchaser_email, :recipient => { :email => recipient_email } } }
   end
 
 end
--- server/spec/pair_generator_spec.rb
+++ server/spec/pair_generator_spec.rb
@@ -23,7 +23,35 @@ describe 'Pair Generator' do
       end
     end
 
-  
+    context 'there are two participants' do
+      it 'pairs the first participant with the second participant' do
+        participants = [{ :email => "first.participant@example.com" }, { :email => "second.participant@example.com" }]
+        actual_pairs = PairGenerator.new.generate_pairs(participants)
+
+        expected_pairs = [
+          { :purchaser =>
+            { :email => "first.participant@example.com", :recipient => { :email => "second.participant@example.com" } }
+          },
+          { :purchaser =>
+            { :email => "second.participant@example.com", :recipient => { :email => "first.participant@example.com" } }
+          }
+        ]
+
+        expect(actual_pairs).to eq(expected_pairs)
+      end
+    end
+
+    context 'there are three participants' do
+      it 'pairs the participants such that each purchaser has a unique recipient' do
+        participants = [{ :email => "first.participant@example.com" }, { :email => "second.participant@example.com" }, { :email => "third.participant@example.com" }]
+        actual_pairs = PairGenerator.new.generate_pairs(participants)
+
+        puts actual_pairs[0]
+        expect(actual_pairs[0][:purchaser][:email]).not_to eq(actual_pairs[0][:purchaser][:recipient][:email])
+        expect(actual_pairs[1][:purchaser][:email]).not_to eq(actual_pairs[1][:purchaser][:recipient][:email])
+        expect(actual_pairs[2][:purchaser][:email]).not_to eq(actual_pairs[2][:purchaser][:recipient][:email])
+      end
+    end
 
   end
 
