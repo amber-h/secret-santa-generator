c23b7994d7c30576c1f1eb60b998c33f407f1f32
Amber Houle <amber.houle3@gmail.com>
2015-11-16 20:11:46 -0500

    Removing mail gem and adding mandrill-api gem, fixing email_sender to use mandrill api

--- server/app.rb
+++ server/app.rb
@@ -1,18 +1,20 @@
 require 'sinatra'
 require 'json'
 require_relative './lib/email_sender'
+require_relative './lib/pair_generator'
 
 class App < Sinatra::Base
-
-  get '/' do
-
-  end
+  set :bind, '0.0.0.0'
+  set :port, 4567
 
   post '/participants' do
     headers 'Access-Control-Allow-Origin' => '*'
+
     data = JSON.parse request.body.read
-    puts "Hello #{data}!"
-    EmailSender.new.send_mail
+    pairs = PairGenerator.new.generate_pairs data
+    EmailSender.new.send_mail pairs
+
+    response
   end
 
 end
--- server/lib/email_sender.rb
+++ server/lib/email_sender.rb
@@ -1,23 +1,36 @@
-require 'mail'
+require 'mandrill'
 
 class EmailSender
 
-  def send_mail
-    Mail.defaults do
-      delivery_method :smtp,
-      :address => "192.168.99.100",
-      :port => 25,
-      :user_name  => 'usr',
-      :password   => 'pwd',
-      :enable_ssl => true
-    end
+  def send_mail pairs
+    pairs.each do |pair|
+      begin
+        mandrill = Mandrill::API.new 'API_KEY'
+        message = build_email pair[:purchaser][:email], pair[:purchaser][:recipient][:email]
+        async = true
+        result = mandrill.messages.send message, async
 
-    Mail.deliver do
-      from    'amber.houle3@gmail.com'
-      to      'amber.houle3@gmail.com'
-      subject 'This is a test email'
-      body    "HELLO TEST"
+      rescue Mandrill::Error => e
+        puts "A mandrill error occurred: #{e.class} - #{e.message}"
+      raise
+      end
     end
   end
+
+  private
+
+def build_email email_recipient, gift_recipient
+  {
+    "from_email"=>"santa@secretsantagenerator.com",
+    "text"=>"Your secret santa is #{gift_recipient}",
+    "to"=>
+      [{"email" => email_recipient,
+        "type" => "to",
+        "name "=> "Recipient Name"}],
+
+    "subject"=>"example subject",
+    "from_name"=>"Example Name"
+  }
+end
 
 end
