99701081a5c27d6e49c3d71bf3c9b7ad47d27a1f
Amber Houle <amber.houle3@gmail.com>
2015-11-22 19:21:25 +0100

    Adding participant name to email sender

--- server/lib/email_sender.rb
+++ server/lib/email_sender.rb
@@ -7,7 +7,7 @@ class EmailSender
 
     pairs.each do |pair|
       begin
-        message = build_email pair[:purchaser][:email], pair[:purchaser][:recipient][:email]
+        message = build_email pair[:purchaser], pair[:purchaser][:recipient]
         async = true
         result = mandrill.messages.send message, async
 
@@ -23,11 +23,12 @@ class EmailSender
 def build_email email_recipient, gift_recipient
   {
     "from_email"=>"santa@secretsantagenerator.com",
-    "text"=>"Disclaimer: Due to current secret santa generator being under construction, there are no available adorable christmas animal photos. Nonetheless, your secret santa is #{gift_recipient} KEEP IT SECRET HAPPY CHRISTMAS!",
+    "text"=>"Disclaimer: Due to current secret santa generator being under construction, there are no available adorable christmas animal photos. Nonetheless, your secret santa is #{gift_recipient[:name]} KEEP IT SECRET HAPPY CHRISTMAS!",
     "to"=>
-      [{"email" => email_recipient,
+      [{"email" => email_recipient[:email],
         "type" => "to",
-        "name "=> email_recipient}],
+        "name "=> email_recipient[:name]
+      }],
 
     "subject"=>"Your Secret Santa has arrived!",
     "from_name"=>"Santa"
