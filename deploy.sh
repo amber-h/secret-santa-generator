02e43279e2142e7060a706af47852e746fef7c41
Amber Houle <amber.houle3@gmail.com>
2015-11-25 22:41:36 +0100

    Changing EB bucket to use the default one created by aws EB

--- deploy.sh
+++ deploy.sh
@@ -1,18 +1,17 @@
 #! /bin/bash
 SHA1=$1
 
-# Deploy image to Docker Hub
 docker push amberhoule/secret-santa-client:$SHA1
 docker push amberhoule/secret-santa-server:$SHA1
 
 # Create new Elastic Beanstalk version
-EB_BUCKET=secret-santa-dockerrun
+EB_BUCKET=elasticbeanstalk-us-east-1-379822063347
 DOCKERRUN_FILE=$SHA1-Dockerrun.aws.json
 sed "s/<TAG>/$SHA1/" < Dockerrun.aws.json.template > $DOCKERRUN_FILE
 aws s3 cp $DOCKERRUN_FILE s3://$EB_BUCKET/$DOCKERRUN_FILE
-aws elasticbeanstalk --region="us-east-1" create-application-version --application-name secret-santa-sender \
-  --version-label $SHA1 --source-bundle S3Bucket=$EB_BUCKET,S3Key=$DOCKERRUN_FILE
+aws elasticbeanstalk create-application-version --application-name secret-santa-sender \
+  --version-label $SHA1 --source-bundle S3Bucket=$EB_BUCKET,S3Key=$DOCKERRUN_FILE --region us-east-1
 
 # Update Elastic Beanstalk environment to new version
-aws elasticbeanstalk --region="us-east-1" update-environment --environment-name staging \
-    --version-label $SHA1
+aws elasticbeanstalk update-environment --environment-name staging \
+    --version-label $SHA1 --region us-east-1
