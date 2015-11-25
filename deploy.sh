9194e570599f00d711a3f9018ec4b15f929c933c
Amber Houle <amber.houle3@gmail.com>
2015-11-25 14:13:11 +0100

    Specifying aws region in deploy script for elastic beanstalk

--- deploy.sh
+++ deploy.sh
@@ -10,9 +10,9 @@ EB_BUCKET=secret-santa-dockerrun
 DOCKERRUN_FILE=$SHA1-Dockerrun.aws.json
 sed "s/<TAG>/$SHA1/" < Dockerrun.aws.json.template > $DOCKERRUN_FILE
 aws s3 cp $DOCKERRUN_FILE s3://$EB_BUCKET/$DOCKERRUN_FILE
-aws elasticbeanstalk create-application-version --application-name secret-santa-sender \
+aws elasticbeanstalk --region="us-east-1" create-application-version --application-name secret-santa-sender \
   --version-label $SHA1 --source-bundle S3Bucket=$EB_BUCKET,S3Key=$DOCKERRUN_FILE
 
 # Update Elastic Beanstalk environment to new version
-aws elasticbeanstalk update-environment --environment-name secret-santa-sender-stg \
+aws elasticbeanstalk --region="us-east-1" update-environment --environment-name secret-santa-sender-stg \
     --version-label $SHA1
